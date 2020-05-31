4472f60c33e9a7d4f8620114ff847f7b45bb97c3
rautamak <timi.rautamaki@gmail.com>
2020-05-31 15:13:20 +0300

    OTAApp: show new update on mainactivity

--- app/src/main/java/org/justcaf/otaapp/MainActivity.java
+++ app/src/main/java/org/justcaf/otaapp/MainActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     TextView txBuild;
     TextView txUpdate;
+    TextView txUpdateTime;
+    LinearLayout updateView;
     FloatingActionButton btnRefresh;
     OTAManager om = new OTAManager();
 
@@ -20,19 +27,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         txBuild = findViewById(R.id.buildText);
         txUpdate = findViewById(R.id.updateText);
+        txUpdateTime = findViewById(R.id.updateTime);
         btnRefresh = findViewById(R.id.refreshButton);
+        updateView = findViewById(R.id.newUpdate);
         btnRefresh.setOnClickListener(new View.OnClickListener() {
                                           @Override
                                           public void onClick(View view) {
                                               refreshUpdate();
                                           }
                                       });
+        txUpdate.setText(R.string.noUpdates);
+        txBuild.setText(getString(R.string.buildText, om.currentVersion));
         refreshUpdate();
         //startService(new Intent(this, CheckOTAService.class));
     }
 
     private void refreshUpdate() {
-        txBuild.setText(getString(R.string.buildText, om.currentVersion));
-        txUpdate.setText(getString(R.string.updateText, om.getVersion()));
+        SimpleDateFormat f = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        Date currentVersion = null;
+        Date updateVersion = null;
+
+        try {
+            currentVersion = f.parse(om.currentVersion);
+            updateVersion = f.parse(om.getUpdateVersion());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        if (updateVersion != null && currentVersion != null) {
+            if (updateVersion.after(currentVersion)) {
+                SimpleDateFormat user = new SimpleDateFormat("dd.MM.yyyy");
+                String userVersion = null;
+                try {
+                    userVersion = user.format(f.parse(om.getUpdateVersion()));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                txUpdate.setVisibility(View.INVISIBLE);
+                txUpdateTime.setText(getString(R.string.updateText, userVersion));
+                updateView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
--- app/src/main/java/org/justcaf/otaapp/OTAManager.java
+++ app/src/main/java/org/justcaf/otaapp/OTAManager.java
@@ -4,28 +4,32 @@ import android.os.Build;
 
 import java.io.IOException;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
 public class OTAManager {
     NetworkController nc = new NetworkController();
-    private String version;
+    private String updateVersion;
     public String currentVersion;
 
+    DateFormat mFormat;
+
     public OTAManager() {
-        DateFormat format = new SimpleDateFormat("dd-MM-yyyy-HH-mm-ss");
-        format.setTimeZone(TimeZone.getTimeZone("CEST"));
-        currentVersion = format.format(Build.TIME);
+        mFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        mFormat.setTimeZone(TimeZone.getTimeZone("CEST"));
+        currentVersion = mFormat.format(Build.TIME);
     }
 
-    public String getVersion() {
-        if (version == null) {
+    public String getUpdateVersion() {
+        if (updateVersion == null) {
             Thread t = new Thread(new Runnable() {
                     @Override
                     public void run() {
                         try {
-                            version = nc.getUpdate();
-                        } catch (IOException e) {
+                            SimpleDateFormat f = new SimpleDateFormat("dd-MM-yyyy-HH-mm-ss");
+                            updateVersion = mFormat.format(f.parse(nc.getUpdate()));
+                        } catch (IOException | ParseException e) {
                             e.printStackTrace();
                         }
                     }
@@ -37,7 +41,7 @@ public class OTAManager {
                 e.printStackTrace();
             }
         }
-        return version;
+        return updateVersion;
     }
 
     public void update() {
