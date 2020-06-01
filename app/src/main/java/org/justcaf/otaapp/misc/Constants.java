b9747b70df0a2449c2afce30dba52522def8574b
rautamak <timi.rautamaki@gmail.com>
2020-06-01 17:46:37 +0300

    OTAApp: enable install button

--- app/src/main/java/org/justcaf/otaapp/misc/Constants.java
+++ app/src/main/java/org/justcaf/otaapp/misc/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
     public static String FILE_DOWNLOAD_NAME = "update.zip";
     public static String OTA_DATA_URL =
             "https://raw.githubusercontent.com/Just-CAF/releases/custom/releases.txt";
+
     public static String CHANGELOG_URL_PREFIX =
             "https://raw.githubusercontent.com/Just-CAF/releases/custom";
 
