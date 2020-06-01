49b5b51185506b591338955547fc06985c2c6efb
rautamak <timi.rautamaki@gmail.com>
2020-06-01 14:52:28 +0200

    OTAApp: add constraintlayout to static libraries

--- Android.mk
+++ Android.mk
@@ -3,8 +3,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := Updater
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+#LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
 LOCAL_USE_AAPT2 := true
 
@@ -12,6 +13,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     com.google.android.material_material \
     androidx.core_core \
     androidx.appcompat_appcompat \
+    androidx-constraintlayout_constraintlayout
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
 LOCAL_REQUIRED_MODULES := privapp_whitelist_org.justcaf.otaapp.xml
